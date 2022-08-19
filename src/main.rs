a35a5799a7222f05b6a3bdfd79d6cd8a2b9bfd28
Birkin James Diana <birkin.diana@gmail.com>
2022-08-19 08:00:28 -0400

    experiments with logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+// #[macro_use]
+extern crate log;
+extern crate env_logger;
+
+// use env_logger::Env;
+// use std::env;
+// use std::process;
+
+
+use log::{debug, info, error};
+
+
+
+
+
+
+fn main() {    
+    env_logger::init();
+
+    debug!( "this is a debug ``{}``", "message" );
+    info!( "this is an info ``{}``", "message" );
+    error!( "this is an error ``{}``", "message" );
+
     println!("Hello, world!");
 }

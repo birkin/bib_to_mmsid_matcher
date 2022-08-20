5659ba05557150d84cbcfaf1c37f36c50ab758bf
Birkin James Diana <birkin.diana@gmail.com>
2022-08-20 17:45:14 -0400

    starts implementing logging-pattern from logrotate project.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,35 @@ extern crate env_logger;
 // use std::process;
 
 
-use log::{debug, info, error};
+// use log::{debug, info, error};
+use env_logger::{Builder, Target};
 
 
 
+#[derive(Deserialize, Debug)]
+struct Config {
+    log_level: String,
+    logger_json_file_path: String,
+    max_entries: i8  // this could be added to the json-file instead
+}
+
+
+impl Config {
+    /*  forgive the "RUST_LOG" hack; i want to use the envar project-prefix to set the log-level,
+        ...and couldn't figure out how to specify an alternative prefix for env_logger::init() */
+    fn new() -> Config {
+        match envy::prefixed("LOG_ROTATOR__").from_env::<Config>() {  // https://github.com/softprops/envy
+            Ok(config) => {
+                env::set_var( "RUST_LOG", &config.log_level);
+                let log_level = config.log_level;  // not used, but still useful to set, for panic-message if it's missing
+                let logger_json_file_path = config.logger_json_file_path;
+                let max_entries = config.max_entries;
+                Config { log_level, logger_json_file_path, max_entries }
+            },
+            Err(error) => panic!("{:#?}", error) // this shows the missing envar
+        }
+    }
+}
 
 
 

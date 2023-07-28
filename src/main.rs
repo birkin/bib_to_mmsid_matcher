c24f6cc6676acff46a2daa2a9546581efb3a04b3
Birkin James Diana <birkin.diana@gmail.com>
2023-07-27 23:14:47 -0400

    adds clap for cli work.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ use env_logger::{Builder, Target};
 use serde::Deserialize;
 use std::env;
 
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    bibnum: String,
+}
+
 #[derive(Deserialize, Debug)]
 struct Config {
     log_level: String,
